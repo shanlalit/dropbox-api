720631d94267d81aa575a8df647460490c30b9fc
Lalit K. Shandilya <shanlalit@geobeats.com>
2013-12-05 11:53:12 +0530

    No longer need to download Image file header

--- lib/dropbox-api/client/files.rb
+++ lib/dropbox-api/client/files.rb
@@ -5,19 +5,6 @@ module Dropbox
 
       module Files
 
-        # Download only the EXIF part, only the first 64kb or 128kb of the image files
-        def download_headers(file, options = {})
-          # Check if file type is image
-          if file.mime_type.include?("image")
-            root     = options.delete(:root) || Dropbox::API::Config.mode
-            path     = Dropbox::API::Util.escape(file.path)
-            url      = ['', "files", root, path].compact.join('/')
-            connection.get_raw(:content, url, {}, {
-              "Range" => "bytes=131072"
-            })
-          end
-        end
-
         def download(path, options = {})
           root     = options.delete(:root) || Dropbox::API::Config.mode
           path     = Dropbox::API::Util.escape(path)
@@ -47,24 +34,24 @@ module Dropbox
           chunk_size = options[:chunk_size] || 4*1024*1024 # default 4 MB chunk size
           offset = options[:offset] || 0
           upload_id = options[:upload_id]
-                    
+
           while offset < total_file_size
             data = file.read(chunk_size)
-            
+
             query    = Dropbox::API::Util.query(options.merge(:offset => offset))
             response = connection.put(:content, "#{upload_url}?#{query}", data, {
               'Content-Type'   => "application/octet-stream",
               "Content-Length" => data.length.to_s
             })
-            
+
             upload = Dropbox::API::Object.init(response, self)
             options[:upload_id] ||= upload[:upload_id]
             offset += upload[:offset].to_i - offset if upload[:offset] && upload[:offset].to_i > offset
             yield offset, upload if block_given?
           end
-          
+
           query = Dropbox::API::Util.query({:upload_id => options[:upload_id]})
-          
+
           response = connection.post(:content, "#{commit_url}?#{query}", "", {
             'Content-Type'   => "application/octet-stream",
             "Content-Length" => "0"
